f022a253017050ab1bb51327d6bcdc06055316f9
Illina Burlace <chloeburlace@gmail.com>
2025-06-27 10:07:04 +0100

    Removed java and kotlin lsps

--- lua/options/lsp.lua
+++ lua/options/lsp.lua
@@ -1,8 +1,4 @@
-vim.lsp.config["java_language_server"] = {
-	cmd = { "/home/illinab/java-language-server/dist/lang_server_linux.sh" }
-}
-
-vim.lsp.enable("java_language_server")
-vim.lsp.enable("kotlin_language_server")
 vim.lsp.enable("csharp_ls")
 vim.lsp.enable("lua_ls")
+
+
