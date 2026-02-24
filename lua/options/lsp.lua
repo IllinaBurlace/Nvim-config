12eb9fb657f675c011dd98b892fb85d53a432fb8
Illina Burlace <chloeburlace@gmail.com>
2026-02-24 07:22:57 +0000

    made java work

--- lua/options/lsp.lua
+++ lua/options/lsp.lua
@@ -1,6 +1,7 @@
 vim.lsp.enable("csharp_ls")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("gdscript")
+vim.lsp.enable("eslint")
 
 vim.lsp.enable("jdtls")
 
---
+++ lua/plugins/javap.lua
@@ -0,0 +1,3 @@
+return {
+	"udalov/javap-vim"
+}
